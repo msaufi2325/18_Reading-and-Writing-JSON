1801d036529ffc3df7c1bee752726e099daf44bc
Saufi <msaufi2325@gmail.com>
2024-01-25 02:53:34 +0900

    Add JSON unmarshalling code to main function

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+)
+
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
+}
+
 func main() {
 	myJson := `
 	[
@@ -16,5 +28,13 @@ func main() {
 			"has_dog": false
 		}
 	]`
+
+	var unmarshalled []Person
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+
+	log.Printf("unmarshalled: %v", unmarshalled)
 
 }
