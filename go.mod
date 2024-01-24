a629505daff0610cbfadfedaef7c1b5729e612f4
Saufi <msaufi2325@gmail.com>
2024-01-25 02:44:07 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module reading-and-writing-JSON
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+	myJson := `
+	[
+		{
+			"fist_name": "Clark",
+			"last_name": "Kent",
+			"hair_color": "black",
+			"has_dog": true
+		},
+		{
+			"fist_name": "Bruce",
+			"last_name": "Wayne",
+			"hair_color": "black",
+			"has_dog": false
+		}
+	]`
+
+}
